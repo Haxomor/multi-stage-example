2de4da489a3d603c2440fa37584ef984d5388285
Haxomor <96199749+Haxomor@users.noreply.github.com>
2023-05-19 09:20:40 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 
-def updateFileContent(String fileName) {
+def updateFileContent(String example.txt) {
     // Bash function to update file content by incrementing it by 1
     sh """
-    content=\$(cat ${fileName})
+    content=\$(cat ${example.txt})
     updatedContent=\$((content + 1))
-    echo \${updatedContent} > ${fileName}
+    echo \${updatedContent} > ${example.txt}
     """
 }
