592a724729cb45223b8abbc1940773f3d3c5bbfc
Haxomor <96199749+Haxomor@users.noreply.github.com>
2023-05-19 09:15:29 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,9 @@
-#!/bin/bash
 
-version="1.0.0"
-version_parts=($(echo "$version" | tr '.' ' '))  # Split the version string into an array
-
-# Extract individual version components
-major="${version_parts[0]}"
-minor="${version_parts[1]}"
-patch="${version_parts[2]}"
-
-# Increment the last digit
-patch=$((patch + 1))
-
-# Construct the updated version string
-updated_version="$major.$minor.$patch"
-
-echo "Updated version: $updated_version"
+def updateFileContent(String fileName) {
+    // Bash function to update file content by incrementing it by 1
+    sh """
+    content=\$(cat ${fileName})
+    updatedContent=\$((content + 1))
+    echo \${updatedContent} > ${fileName}
+    """
+}
